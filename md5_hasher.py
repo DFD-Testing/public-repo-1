52875ab237a73678fa0b3cd939372cceb4e5bf83
Pratik Garai <58355527+PratikGarai@users.noreply.github.com>
2023-10-13 12:40:25 +0530

    Create md5_hasher.py

---
+++ md5_hasher.py
@@ -0,0 +1,19 @@
+import hashlib
+
+# Function to generate MD5 hash
+def generate_md5_hash(text):
+    # Create an MD5 hash object
+    md5_hash = hashlib.md5()
+
+    # Update the hash object with the bytes-like object of the input string
+    md5_hash.update(text.encode('utf-8'))
+
+    # Get the hexadecimal representation of the hash
+    md5_hash_hex = md5_hash.hexdigest()
+
+    return md5_hash_hex
+
+# Example usage
+password = input()
+secure_hash = generate_md5_hash(input_text)
+print("Secure Hash:", md5_hash)
